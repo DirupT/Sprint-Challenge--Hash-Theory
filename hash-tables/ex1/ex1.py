8967d4c0770b40e21fa8dcbebb853bf533932bdb
dirupt <dirupt@DESKTOP-HEUGRKD.localdomain>
2018-10-12 10:05:20 -0500

    init

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,7 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+    pass
+
 
 if __name__ == '__main__':
-  # You can write code here to test your implementation using the Python repl
-  pass 
+    # You can write code here to test your implementation using the Python repl
+    pass
