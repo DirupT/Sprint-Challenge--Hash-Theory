0e6f12f94973e795e3dcd220f80409aed235c7ba
dirupt <dirupt@DESKTOP-HEUGRKD.localdomain>
2018-10-12 10:41:24 -0500

    finished ex1 of hash-tables

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,7 +1,17 @@
 def get_indices_of_item_weights(weights, limit):
-    pass
+    hash_table = {}
+    for index, key in enumerate(weights):
+        hash_table[key] = index
+
+    for i, weight in enumerate(hash_table):
+        try:
+            return (hash_table[limit - weight], i)
+        except KeyError:
+            print("error")
+
+    return ()
 
 
 if __name__ == '__main__':
-    # You can write code here to test your implementation using the Python repl
-    pass
+        # You can write code here to test your implementation using the Python repl
+    print(get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40], 7))
